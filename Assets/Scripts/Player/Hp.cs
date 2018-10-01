2b0af9a031f91b1f4e14d864440f38b590cbeba8
valli22 <davidvacasmiguel@hotmail.com>
2018-10-01 17:43:13 +0200

    Hp

---
+++ Assets/Scripts/Player/Hp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hp : MonoBehaviour {
+
+	float maxHp, currentHp = 100;
+
+	public bool canTakeDame = true;
+	public Image healthBar;
+	PlayerController pController;
+
+	// Use this for initialization
+	void Start () {
+		pController = GetComponent<PlayerController> ();
+		if (pController == null)
+			Debug.LogError ("PlayerController not founded!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TakeDamage(float damage){
+		if (!canTakeDame)
+			return;
+		
+		currentHp -= damage;
+		//Death
+		if (currentHp <= 0) {
+		
+		}
+
+		healthBar.fillAmount = currentHp / maxHp;
+
+	}
+}
